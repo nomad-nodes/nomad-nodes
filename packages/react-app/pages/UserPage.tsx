2e8fc424189182f74d7d0d9ddb4f7167d00e5c08
mariomui <yi.kan.mui@gmail.com>
2022-03-20 07:34:50 -0700

    Generate wifibars.

--- packages/react-app/pages/UserPage.tsx
+++ packages/react-app/pages/UserPage.tsx
@@ -1,37 +1,61 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
-const getWifiStrength = getRandomIntFactory(1,6);
+import { flexbox } from "@mui/system";
+const getWifiStrength = getRandomIntFactory(1, 6);
 const wifis = [
   {
-    name:'mariowifi',
+    name: 'dorothy_wifi',
     strength: getWifiStrength()
   },
   {
-    name:'jasonwifi',
+    name: 'yellow_brick_road_wifi',
     strength: getWifiStrength()
   },
   {
-    name:'jimWifi',
+    name: 'dropped_house_wifi',
     strength: getWifiStrength()
   }
 ];
-function WifiCard({name, strength}) {
+
+const oneBar = (width, height, isFilled = false) => {
+  return (<div style={{
+    width,
+    height: `${10 * height}px`,
+    border: '1px solid black',
+    background: `${isFilled ? 'black' : 'unset'}`
+  }}
+  ></div>)
+}
+const FilledBar = ({ numFilled, numMax, width, oneBar }) => {
+  let counter = numFilled
+  const bars = Array(numMax).fill(null).map((item, idx) => {
+    counter--;
+    console.log(counter);
+    const isFilled = (counter > -1) === true
+    return () => oneBar(width / numMax, idx, isFilled)
+  })
+  return <div style={{ display: 'flex' }}>
+    {bars.map((Barcmp) => Barcmp())}
+  </div>
+}
+function WifiCard({ name, strength }) {
   return (<Card sx={{ width: "50%", minWidth: '384px' }}>
-      <CardHeader title={name} />
-      <CardContent>
-        {strength}
-      </CardContent>
+    <CardHeader title={name} />
+    <CardContent>
+      {strength}
+      <FilledBar numFilled={strength} numMax={5} width={100} oneBar={oneBar} />
+    </CardContent>
   </Card>)
 }
 export default function UserPage() {
   return <ul style={{
     listStyleType: "none",
     display: "flex",
-    flexDirection:"column"
+    flexDirection: "column"
   }}>
     {
-      wifis.map(({name,strength}) => {
-        return (<li style={{marginTop: ".2em"}}>
-         <WifiCard name={name} strength={strength} />
+      wifis.map(({ name, strength }) => {
+        return (<li style={{ marginTop: ".2em" }}>
+          <WifiCard name={name} strength={strength} />
         </li>)
       })
     }
