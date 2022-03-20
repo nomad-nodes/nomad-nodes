043684fbcd5ce6563c320c2c5be70dddb1a3e6e3
mariomui <yi.kan.mui@gmail.com>
2022-03-20 05:32:06 -0700

    Glue everything.

--- owner-login-page/src/components/pages/StatisticsPage.js
+++ owner-login-page/src/components/pages/StatisticsPage.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from 'react';
+// import { Link } from 'react-router-dom'
 
 //import DataUsageImage from '../../assets/images/GlassWire_Data_Usage.webp'
 //import FirewallImage from '../../assets/images/GlassWire_Firewall_Software.webp'
 
-import '../../App.css'
+import '../../App.css';
 
 export default function StatsPage() {
-
-    return (
-        <div>
-            <img src = {require('../../assets/images/firewall.png')} alt = "Firewall" width="700" height="630"/>
-            <img src = {require('../../assets/images/data_usage.png')} alt = "Data Usage" width="730" height="630"/>
-        </div>
-
-    )
-
+  return (
+    <div style={{ height: '100vh' }}>
+      <iframe
+        width="100%"
+        height="100%"
+        title="stats"
+        src="http://localhost:4000"
+      />
+      {/* <img src = {require('../../assets/images/firewall.png')} alt = "Firewall" width="700" height="630"/> */}
+      {/* <img src = {require('../../assets/images/data_usage.png')} alt = "Data Usage" width="730" height="630"/> */}
+    </div>
+  );
 }
